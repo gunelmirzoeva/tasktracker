390d6001e4ce1351f97ce3e8eb4b94b5c8e5ce6b
gunelmirzoeva <mirzoevagunel20@gmail.com>
2025-08-01 16:56:49 +0400

    Added Task entity with JPA auditing

--- src/main/java/com/example/tasktracker/TasktrackerApplication.java
+++ src/main/java/com/example/tasktracker/TasktrackerApplication.java
@@ -2,8 +2,10 @@ package com.example.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TasktrackerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/tasktracker/entity/Task.java
@@ -0,0 +1,38 @@
+package com.example.tasktracker.entity;
+
+import com.example.tasktracker.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
